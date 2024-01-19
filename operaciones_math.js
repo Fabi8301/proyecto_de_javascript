4f9c11ef87dc43bd40df421a59da16ff34eedcff
fabi8301 <fabianregunega83@gmail.com>
2024-01-19 18:15:29 -0300

    se realizaran cambios

--- operaciones_math.js
+++ operaciones_math.js
@@ -80,13 +80,20 @@ alert("hola, puedes ingresar 2 numeros para hallar la ipotenusa")
 /* intento que el calculo sea de:
 c = a2 + b2 */
 
-let ladoA = prompt("ingrese su primer numero")
-let ladoB = prompt("ingrese el siguiente numero")
-let x = Number (ladoA)
-let y = Number(ladoB)
+let ladoA = prompt("ingrese su primer numero");
+let ladoB = prompt("ingrese el siguiente numero");
+let a = Number (ladoA);
+let b = Number(ladoB);
+let  hipo = 0;
+if (a && b){
+    hipo = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+    console.log(`el resultado de la hipotenusa es = ${hipo}` );
+}
+else {
+     console.log("ingrese un numero");
+}
 
 
-let hipo = Math.sqrt(Math.pow(ladoA, 2) + Math.pow(ladoB, 2))
 
 
 
