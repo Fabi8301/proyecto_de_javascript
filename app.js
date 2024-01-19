63d882da252d1e155c47c6a20405590a4869630b
fabi8301 <fabianregunega83@gmail.com>
2024-01-19 18:46:50 -0300

    new cambios

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let a = 10
 console.log(a);
+// aqui esta el uso de los numeros 
 Math.random()
 console.log(Math.random() * 100)
 console.log(Math.floor(Math.random()* 100))
--- operaciones_math.js
+++ operaciones_math.js
@@ -74,24 +74,6 @@ console.log(cnr1);
 let cnr2 = 4;
 cnr2--;
 console.log(cnr2);
-/*----------------------------------------------------------------------------------*/
-console.log("aqui esta la investigacion de el teorema de pitagoras")
-alert("hola, puedes ingresar 2 numeros para hallar la ipotenusa")
-/* intento que el calculo sea de:
-c = a2 + b2 */
-
-let ladoA = prompt("ingrese su primer numero");
-let ladoB = prompt("ingrese el siguiente numero");
-let a = Number (ladoA);
-let b = Number(ladoB);
-let  hipo = 0;
-if (a && b){
-    hipo = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
-    console.log(`el resultado de la hipotenusa es = ${hipo}` );
-}
-else {
-     console.log("ingrese un numero");
-}
 
 
 
