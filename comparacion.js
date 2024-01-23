1c255db68b52d52a86c576dfd32c3b5686410100
fabi8301 <fabianregunega83@gmail.com>
2024-01-23 15:08:19 -0300

    cambio para orden

---
+++ comparacion.js
@@ -0,0 +1,18 @@
+let numx = 5;
+let numy = "5";
+console.log(x === y);
+
+let nx = 5;
+let ny = "5";
+console.log(x != y);
+
+let x = 5;
+let y = "5";
+console.log(x !== y);
+
+//mayor y menor que
+
+let nux = 5;
+let nuy = 6;
+console.log(x > y);
+
---
+++ logicas.js
@@ -0,0 +1,17 @@
+//utilizacion de and: &&
+let a = 1;
+let b = 2;
+let c = 3;
+console.log(a < b && b < c);
+
+//utilizacion de or:||
+console.log(a < b || b < c);
+
+//utilizacion de not: !
+let n = false;
+console.log(!n); // retorna true
+
+let x = 1;
+let y = 2;
+console.log(!(x < y));
+
