798673c70884396ead03159b722e69a67cee8463
fabi8301 <fabianregunega83@gmail.com>
2024-01-23 15:36:56 -0300

    cambios de orden

--- array.js
+++ array.js
@@ -2,7 +2,18 @@
 let variable = "hola"
 
 //arrays
-let arr = ["tomate, locote, cebolla, papa"];
+let arr = ["tomate","locote","cebolla","papa"];
 
 console.log(arr.length);
+console.log(arr[0]);
+console.log(arr[1]);
 console.log(arr[2]);
+console.log(arr[3]);
+
+console.log(arr[5]);
+console.log(arr[-1]);
+arr[5] = "banana";
+arr[-1] = "repollo";
+
+console.log(arr[5]);
+console.log(arr[-1]);
--- comparacion.js
+++ comparacion.js
@@ -16,3 +16,46 @@ let nux = 5;
 let nuy = 6;
 console.log(x > y);
 
+//aqui esta un ejercicio pendiente
+/*
+const c = "5";
+//¿Qué tipo de dato tiene la siguente variable? const c = 91;
+//¿Cuál de los dos es mejor?, línea 1 or línea 2?
+let empty1 = undefined; //line 1
+let empty2 = null; //line 2
+//¿Qué salida producen en consola?
+let a = "Hello";
+a = "world";
+console.log(a);
+//¿Qué se mostrará en la consola?
+let a = "world";
+let b = `Hello ${a}!`;
+console.log(b);
+//¿Cuál es el valor de a?
+let a = "Hello";
+a = prompt("world");
+console.log(a);
+//¿Cuál es el valor de b en la consola?
+let a = 5;
+let b = 70;
+let c = "5";
+b++;
+console.log(b);
+//¿Cuál es el valor de este resultado? let result = 3 + 4 * 2 / 8;
+//¿Cuál es el valor de total y total2?
+let firstNum = 5;
+let secondNum = 10;
+firstNum++;
+secondNum--;
+let total = ++firstNum + secondNum;
+console.log(total);
+let total2 = 500 + 100 / 5 + total--;
+console.log(total2);
+//¿Qué se mostrará en la consola aquí?
+const a = 5;
+const b = 10;
+console.log(a > 0 && b > 0);
+console.log(a == 5 && b == 4);
+console.log(true ||false);
+console.log(a == 3 || b == 10);
+console.log(a == 3 || b == 7);*/
