d42ac7b36aa74ad3740286b4cab6e43440319501
fabi8301 <fabianregunega83@gmail.com>
2024-01-24 19:40:33 -0300

    cambios poximos atento

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
-//variables normales y sus tipos
-let variable = "hola"
+//variables normales y sus tipos;
+let variable = "hola";
 
-//arrays
+//arrays, lista de verduras?
 let arr = ["tomate","locote","cebolla","papa"];
 
 console.log(arr.length);
@@ -14,6 +14,14 @@ console.log(arr[5]);
 console.log(arr[-1]);
 arr[5] = "banana";
 arr[-1] = "repollo";
-
+console.log(arr.length);
 console.log(arr[5]);
 console.log(arr[-1]);
+
+arr.push("peregil");
+console.log(arr[6]);
+
+const semilla1 = arr.splice(0,2); 
+const semilla2 = arr[3]
+const mezcla = semilla1.concat(semilla2)
+console.log(mezcla);
---
+++ splese.js
@@ -0,0 +1,14 @@
+//el codigo de splece para agregar valores al array 
+let embutido = ["chorizo", "panchos","fiambre"];
+console.log(embutido, embutido.length);
+embutido.push("peperoni");
+console.log("el embutido 3 es", embutido[2]);
+embutido.push("peperoni");
+embutido.splice(2, 0, "carne", "lechuga");
+
+console.log(embutido)
+//el codigo de shift para eliminar valores de forma lineal 
+let arr = ["manzana","banana", "naranja", "limon"]
+console.log(arr, arr.length)
+arr.shift(1, 1)
+console.log(arr);
