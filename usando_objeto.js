81a01eee65fcf5944128b5f0a0c1a9bc3b8f0640
fabi8301 <fabianregunega83@gmail.com>
2024-01-28 22:13:32 -0300

    new cambio

--- usando_objeto.js
+++ usando_objeto.js
@@ -1,4 +1,20 @@
-const person = {
-    pelo: ""
-    
+
+//tipos de animales domesticos
+class Animal {
+     constructor (nombre, sexo, color, altura) {
+        this.nombre = nombre;
+        this.sexo = ["Macho", "Hembra", "hermafrodita"];
+        this.color = color;
+        this.altura = altura
+        this.grupo
+    }
+    grupo(grupo) {
+        if(grupo){
+            this.grupo.push(grupo)
+        }
+    }
+
+
+      
+
 }
